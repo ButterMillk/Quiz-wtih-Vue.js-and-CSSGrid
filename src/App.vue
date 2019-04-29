c074e7fe55390b9d27ba9d825d34a66715d842ce
Mariuszek <maslanka.mario@gmail.com>
2019-04-29 10:32:43 +0200

    First Commit in this Project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <p :text="this.textInput">Siema {{textInput}}</p>
+      <Question />
+  </div>
+</template>
+
+<script>
+
+import Question from './components/Question.vue';
+export default {
+  name: 'App',
+
+  data() {
+    return{
+      textInput: '',
+    };
+  },
+
+  components: {
+    Question,
+  },
+
+
+}
+</script>
+
+<style>
+#app{
+  box-sizing: border-box;
+
+}
+
+</style>
---
+++ src/components/Question.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="wrapperQuestion">
+        <p>Tutaj będzie pytanie</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Question',
+}
+</script>
+
+<style scoped>
+.wrapperQuestion{
+    box-sizing: border-box;
+}
+
+</style>
